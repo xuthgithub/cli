a493d7410e178f3fd164a0b98261f43dde863a8b
vilmibm <vilmibm@neongrid.space>
2021-03-15 16:06:43 -0500

    some view copypasta

--- pkg/cmd/workflow/view/view.go
+++ pkg/cmd/workflow/view/view.go
@@ -2,12 +2,16 @@ package view
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/cli/cli/internal/ghrepo"
+	"github.com/cli/cli/pkg/cmd/workflow/shared"
 	"github.com/cli/cli/pkg/cmdutil"
 	"github.com/cli/cli/pkg/iostreams"
+	"github.com/cli/cli/pkg/markdown"
 	"github.com/spf13/cobra"
 )
 
@@ -20,6 +24,7 @@ type ViewOptions struct {
 	YAML       string
 
 	Prompt bool
+	Raw    bool
 }
 
 func NewCmdView(f *cmdutil.Factory, runF func(*ViewOptions) error) *cobra.Command {
@@ -43,6 +48,8 @@ func NewCmdView(f *cmdutil.Factory, runF func(*ViewOptions) error) *cobra.Comman
 			// support `-R, --repo` override
 			opts.BaseRepo = f.BaseRepo
 
+			opts.Raw = !opts.IO.CanPrompt()
+
 			if len(args) > 0 {
 				opts.WorkflowID = args[0]
 			} else if !opts.IO.CanPrompt() {
@@ -63,6 +70,54 @@ func NewCmdView(f *cmdutil.Factory, runF func(*ViewOptions) error) *cobra.Comman
 
 func runView(opts *ViewOptions) error {
 	// TODO replace this with just the yaml view
+
+	workflowID := opts.WorkflowID
+	var workflow *shared.Workflow
+
+	fmt.Printf("DBG %#v\n", workflowID)
+	fmt.Printf("DBG %#v\n", workflow)
+
+	if opts.Prompt {
+		// TODO grab workflows
+		// TODO prompt
+	}
+
+	if workflow == nil {
+		// TODO REST GET workflowID
+	}
+
+	// TODO figure out how to get the yaml at default branch of remote
+	// TODO lay out yaml with syntax highlighting
+
+	yaml := "# TODO YAML"
+
+	theme := opts.IO.DetectTerminalTheme()
+	markdownStyle := markdown.GetStyle(theme)
+	if err := opts.IO.StartPager(); err != nil {
+		fmt.Fprintf(opts.IO.ErrOut, "starting pager failed: %v\n", err)
+	}
+	defer opts.IO.StopPager()
+
+	if !opts.Raw {
+		// TODO print little header
+		codeBlock := fmt.Sprintf("```yaml\n%s\n```", yaml)
+		// TODO fix indentation
+		rendered, err := markdown.Render(codeBlock, markdownStyle, "")
+		if err != nil {
+			return err
+		}
+		_, err = fmt.Fprint(opts.IO.Out, rendered)
+		return err
+	}
+
+	if _, err := fmt.Fprint(opts.IO.Out, yaml); err != nil {
+		return err
+	}
+
+	if !strings.HasSuffix(yaml, "\n") {
+		_, err := fmt.Fprint(opts.IO.Out, "\n")
+		return err
+	}
 
 	return nil
 }
